745c604e7721269251e97193dc5586c97ea62434
PanosKons <p.konstantopoulos@hotmail.com>
2025-06-23 13:55:16 +0300

    rendercom prof

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import notesRoutes from "./routes/notesRoutes";
 import cors from "cors";
 import { connectDB } from "./config/db";
@@ -8,7 +8,9 @@ import path from "path";
 
 dotenv.config();
 const app = express();
+const PORT: string = process.env.PORT!;
 const __dirname = path.resolve();
+
 if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
@@ -23,12 +25,10 @@ app.use("/api/notes", notesRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("/", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
-
-const PORT: string = process.env.PORT!;
 
 connectDB().then(() => {
   app.listen(Number(PORT), () => {
