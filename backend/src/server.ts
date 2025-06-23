f7930bb814e22147b89e5c1a638ff2e7913eab4c
PanosKons <p.konstantopoulos@hotmail.com>
2025-06-23 11:10:05 +0300

    prepared for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,32 @@
 import express, { json } from "express";
 import notesRoutes from "./routes/notesRoutes";
+import cors from "cors";
 import { connectDB } from "./config/db";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter";
+import path from "path";
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 //app.use(rateLimiter); //go to upstash website + env variables
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 const PORT: string = process.env.PORT!;
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import axios, { AxiosError } from "axios";
+import NoteCard, { type Note } from "../components/NoteCard";
+import api from "../lib/axios";
+import NotesNotFound from "../components/NotesNotFound";
 
 const homePage = () => {
+  const [isRateLimited, setRateLimited] = useState(false);
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await api.get("/notes");
+        setNotes(res.data);
+        setRateLimited(false);
+      } catch (error) {
+        console.error(error);
+        const axiosError = error!;
+        if (axiosError instanceof AxiosError) {
+          if (axiosError.response?.status === 429) setRateLimited(true);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
   return (
     <div className="min-h-screen">
       <NavBar />
+      {isRateLimited && <RateLimitedUI />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
